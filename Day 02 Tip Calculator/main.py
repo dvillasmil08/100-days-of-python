fee8798bda38ff97a40c5b2e2b63b9b854a0e51f
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-23 14:03:30 -0400

    finished day 2, tip caculator

--- Day 02 Tip Calculator/main.py
+++ Day 02 Tip Calculator/main.py
@@ -1,7 +1,13 @@
-two_digit_number = input("Type a two digit number: ")
+# If the bill was $150.00, split between 5 people, with 12% tip.
 
-first_digit = int(two_digit_number[0])
-second_digit = int(two_digit_number[1])
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
 
-result = first_digit + second_digit
-print(result)
+bill_with_tip = tip / 100 * bill + bill
+final_bill = round(bill_with_tip / people, 2)
+final_bill = "{:.2f}".format(final_bill)
+print(f"Each person should play: ${final_bill}")
