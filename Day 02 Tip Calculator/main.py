20b07bd5d8b8f93cf28e4e17d33e421ab5d87091
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-25 20:18:18 -0400

    basic if/else statement

--- Day 02 Tip Calculator/main.py
+++ Day 02 Tip Calculator/main.py
@@ -1,7 +1,26 @@
-two_digit_number = input("Type a two digit number: ")
+# two_digit_number = input("Type a two-digit number: ")
+#
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+#
+# result = first_digit + second_digit
+# print(result)
 
-first_digit = int(two_digit_number[0])
-second_digit = int(two_digit_number[1])
+# Mathematical Operations in Python
+# 3 + 5  Addition
+# 7 - 4  Subtraction
+# 3 * 2  Multiplication
+# 6 / 3  Division
+# 2 ** 3  Exponent
 
-result = first_digit + second_digit
-print(result)
+# PEMDASLR
+# Left to Right
+# ()
+# **
+# * /
+# + -
+
+# print(3 * 3 + 3 / 3 - 3)  # 7.0
+# print(3 * (3 + 3) / 3 - 3)  # 3.0
+
+
---
+++ Day 03 Treasure Island/main.py
@@ -0,0 +1,13 @@
+# 3.1
+# if condition:
+#   do this
+# else:
+#   do this
+
+print("Welcome to the roller coaster!")
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+    print("You can ride the roller coaster!")
+else:
+    print("You cannot ride the roller coaster!")
