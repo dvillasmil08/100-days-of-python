4fabefb54941d037ef5c171b1274064b18aca20b
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-26 12:18:57 -0400

    Created a multiple option game. Basic if/elif/else logic

--- Day 03 Treasure Island/main.py
+++ Day 03 Treasure Island/main.py
@@ -1,13 +1,47 @@
-# 3.1
-# if condition:
-#   do this
-# else:
-#   do this
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
-print("Welcome to the roller coaster!")
-height = int(input("What is your height in cm?"))
+first_choice = input("You\'re at a crossroad, where do you want to go? Type \"left\" or \"right\". ").lower()
 
-if height >= 120:
-    print("You can ride the roller coaster!")
+if first_choice == "left":
+    second_choice = input(
+        "You\'ve come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type "
+        "\"swim\" to swim across. ").lower()
+    if second_choice == "wait":
+        third_choice = input(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which "
+            "colour do you choose? ").lower()
+        if third_choice == "blue":
+            print("Eaten by beats. Game Over.")
+        elif third_choice == "yellow":
+            print("You Win!")
+        elif third_choice == "red":
+            print("Burned by fire. Game Over")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by a trout. Game Over.")
 else:
-    print("Sorry, you have to grow taller to ride the roller coaster!")
+    print("You fell into a hole. Game Over")
