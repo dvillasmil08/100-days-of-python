d3feec9b04da63af153567367c1cb36ac6e7d1b3
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-29 22:29:18 -0400

    made a treasure map based on user input

--- Day 04 Rock, Paper, Scissors Game/main.py
+++ Day 04 Rock, Paper, Scissors Game/main.py
@@ -27,7 +27,20 @@ import random
 
 # Nested Lists
 
-fruits = ["strawberries", "nectarines", "apples"]
-vegetables = ["spinach", "tomato", "potatoes"]
-dirty_dozen = [fruits, vegetables]
-print(dirty_dozen)
+# Treasure map
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+selected_row = map[vertical - 1]
+selected_row[horizontal -1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
+
+
