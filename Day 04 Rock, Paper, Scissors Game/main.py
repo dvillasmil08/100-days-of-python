42442bde3ef48e742226a4c109936e9bd7df5a7e
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-29 21:46:48 -0400

    created logic to pic random without random.choice

--- Day 04 Rock, Paper, Scissors Game/main.py
+++ Day 04 Rock, Paper, Scissors Game/main.py
@@ -1,8 +1,24 @@
+# import random
+#
+# coin_flip = random.randint(0, 1)
+#
+# if coin_flip == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+#
+# List
+# fruits = [item1, item2]
+
+# Import the random module here
 import random
 
-coin_flip = random.randint(0, 1)
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
 
-if coin_flip == 1:
-    print("Heads")
-else:
-    print("Tails")
+# Total items in list
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+person_to_pay = names[random_choice]
+print(person_to_pay + " is going to buy the meal today.")
