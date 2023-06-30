f19501c7ee88d4965901476fda216ff3bd90d310
dvillasmil08 <dvillasmil08@gmail.com>
2023-06-29 22:04:32 -0400

    nested list example

--- Day 04 Rock, Paper, Scissors Game/main.py
+++ Day 04 Rock, Paper, Scissors Game/main.py
@@ -12,15 +12,22 @@
 
 import random
 #
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
 #
 # # Total items in list
-num_items = len(names)
+# num_items = len(names)
 #
 # random_choice = random.randint(0, num_items - 1)
 # person_to_pay = names[random_choice]
 
 # With random.choice
-person_to_pay = random.choice(names)
-print(person_to_pay + " is going to buy the meal today.")
+# person_to_pay = random.choice(names)
+# print(person_to_pay + " is going to buy the meal today.")
+
+# Nested Lists
+
+fruits = ["strawberries", "nectarines", "apples"]
+vegetables = ["spinach", "tomato", "potatoes"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
